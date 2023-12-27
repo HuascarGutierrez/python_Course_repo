78a61e49a3ef2001d248b021e258fce6c04ea994
HuascarGutierrez <huascargutierrezcastro9951591@gmail.com>
2023-12-26 20:33:02 -0400

    finishing methods

--- methods/array_methods.py
+++ methods/array_methods.py
@@ -33,3 +33,33 @@ print(lista_completa)
 # tambien lo puedes usar para borrar un elemento segun el indice que le pongas, empezando por el 0 o tambien de reversa -1
 lista_completa.pop(1)
 print(lista_completa)
+
+#el metodo remove(elemento_a_borrar) es un metodo que borra el primer elemento identico dentro de una lista, si el dato no 
+#se encuentra en la lista, entonces lanza una exception
+lista_ejemplo = ['hola','hola','adios','hola','adios','hola','adios']
+lista_ejemplo.remove('adios')
+print(lista_ejemplo)
+
+#el metodo clear() elimina todos los elementos de una lista
+lista_ejemplo.clear()
+print(lista_ejemplo)
+
+# el metodo .sort() es un metodo que ayuda al ordemnamiento de forma ascendente
+lista = ['adsdf','absd','c','z','f','d']
+lista2 = [56,8,2,-5,1]
+lista.sort()
+lista2.sort()
+print(lista)
+print(lista2)
+#para este metodo podemos hacer un cambio con el reverse de la siguiente forma para cambiar el sentido
+lista2.sort(reverse=True)
+print(lista2)
+
+# .reverse() es un metodo que sirve para invertir el sentido de la lista
+lista = [1,2,3,4,5]
+lista.reverse()
+print(lista)
+
+#al igual que para un string, tambien se puede usar un .index(elemento a encontrar) [5,4,3,2,1] por el reverse
+indice = lista.index(4)
+print(indice)
---
+++ methods/dictionary_methods.py
@@ -0,0 +1,11 @@
+diccionario = {
+    'nombre' : 'Huascar',
+    'apellido' : 'Gutierrez',
+    'id' : 9951591,
+    'nacionalidad' : 'BO'
+}
+
+#el metodo .keys() nos permite ver las keys que tenemos en un diccionario
+claves = diccionario.keys()
+print(claves)
+
